dd277d418a30a3e79f9435bca0f458c8abd6224f
forbesfive <forbesfive@hotmail.com>
2021-08-05 20:49:06 +0100

    getting dataType passed to preview

--- js/script.js
+++ js/script.js
@@ -40,6 +40,21 @@ let objCalculator = {
     },
     preview:function(data){
         console.log(data);
+        let dataType = 'number';
+        switch(data){
+            case '+':
+            case '-':
+            case '/':
+            case '*':
+                dataType = 'operator';
+            break;
+        }
+        console.log(dataType);
+        if(dataType == 'number'){
+            // Do number logic
+        } else {
+            // Do operator logic
+        }
     },
     clear:function(){
         this.number1 = "";
